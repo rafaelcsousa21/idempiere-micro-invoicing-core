ef3f199c1412637204fd07c50b6343508772c2e7
davidpodhola <david@podhola.net>
2019-03-20 18:44:10 +0100

    fix params

--- src/main/kotlin/org/compiere/accounting/BaseDefaultDocumentFactory.kt
+++ src/main/kotlin/org/compiere/accounting/BaseDefaultDocumentFactory.kt
@@ -8,9 +8,12 @@ import org.idempiere.common.util.Env
 import software.hsharp.core.util.DB
 import software.hsharp.core.util.queryOf
 
+/**
+ * Base functionality to [getDocument] by [recordId].
+ */
 abstract class BaseDefaultDocumentFactory : IDocFactory {
-    override fun getDocument(`as`: I_C_AcctSchema?, AD_Table_ID: Int, Record_ID: Int): IDoc? {
-        val tableName = MTable.getDbTableName(Env.getCtx(), AD_Table_ID)
+    override fun getDocument(accountingSchema: I_C_AcctSchema?, tableId: Int, recordId: Int): IDoc? {
+        val tableName = MTable.getDbTableName(Env.getCtx(), tableId)
         //
         val sql = StringBuffer("SELECT * FROM ")
             .append(tableName)
@@ -19,8 +22,8 @@ abstract class BaseDefaultDocumentFactory : IDocFactory {
             .append("_ID=? AND Processed='Y'")
 
         val query =
-            queryOf(sql.toString(), listOf(Record_ID))
-            .map { row -> getDocument(`as`, AD_Table_ID, row) }.asSingle
+            queryOf(sql.toString(), listOf(recordId))
+            .map { row -> getDocument(accountingSchema, tableId, row) }.asSingle
         return DB.current.run(query)
     }
 }
